f3b9243d5b7010b7db8cf251608f63f068c0a367
KanizFatimaMitu <kanizfatima528@gmail.com>
2022-10-28 00:28:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.urlencoded({ extended: true }))
+app.set("view engine", "ejs");
+
+let pLanguages = [];
+
+app.get('/', (req, res) => {
+    res.render('index', {plNames : pLanguages})
+})
+
+app.post('/', (req, res) => {
+   const pLanguage = req.body.pLanguage
+   pLanguages.push(pLanguage)
+   res.redirect('/')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
